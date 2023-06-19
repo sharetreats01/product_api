8a9bbb49d8b5aca8a5edd7e647874e9fde8a7ebe
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-06-19 11:04:13 +0900

    git init

---
+++ src/main/java/com/sharetreats01/sharetreats_product/SharetreatsProductApplication.java
@@ -0,0 +1,13 @@
+package com.sharetreats01.sharetreats_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharetreatsProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharetreatsProductApplication.class, args);
+    }
+
+}
